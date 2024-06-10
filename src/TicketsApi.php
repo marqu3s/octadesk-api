c884399f9c7919468a6cea167f29b9653dced103
João Marques <joao@jjmf.com>
2024-06-10 11:04:09 -0300

    chore: constants for all ticket status

--- src/TicketsApi.php
+++ src/TicketsApi.php
@@ -14,12 +14,19 @@ namespace marqu3s\octadeskApi;
 class TicketsApi extends OctadeskApi
 {
     const TICKET_STATUS_NEW = 'novo';
+    const TICKET_STATUS_NEW_UUID = 'ab59f270-ccb3-4eac-b4e3-5f2d23961dba';
     const TICKET_STATUS_PENDING = 'pendente';
+    const TICKET_STATUS_PENDING_UUID = '09350a7d-886d-4490-ae88-f3a3605ff587';
     const TICKET_STATUS_SOLVED = 'resolvido';
+    const TICKET_STATUS_SOLVED_UUID = 'b2c80a95-840c-4cd0-baa3-f266f303d626';
     const TICKET_STATUS_OPEN = 'andamento';
-    const TICKET_STATUS_OPEN_UUID = '18d082d0-4a24-4620-9d96-a827323dfc8b';
+    const TICKET_STATUS_OPEN_UUID = 'b2c80a95-840c-4cd0-baa3-f266f303d626';
+    const TICKET_STATUS_HOLD = 'em espera';
+    const TICKET_STATUS_HOLD_UUID = '8543caa3-23d2-47f0-bb60-7ccfba62e7b4';
     const TICKET_STATUS_REJECTED = 'rejeitado';
+    const TICKET_STATUS_REJECTED_UUID = '62c696e4-ef22-314a-9f84-5c09153db659';
     const TICKET_STATUS_CANCELED = 'cancelado';
+    const TICKET_STATUS_CANCELED_UUID = '0cac3ff6-6039-4581-a727-c4dff94d5367';
 
     /**
      * @param array $filters
